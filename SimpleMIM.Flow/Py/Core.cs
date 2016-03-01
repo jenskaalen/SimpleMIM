e7ab4402809cd9a46acdcd939824c56573f7eccb
Jens Andre Kålen <ja.kaalen@gmail.com>
2016-03-01 19:01:08 +0100

    Python runspaces will now use lazy loading

--- SimpleMIM.Flow/Py/Core.cs
+++ SimpleMIM.Flow/Py/Core.cs
@@ -8,31 +8,45 @@ namespace SimpleMIM.Flow.Py
         private static ScriptEngine _engine;
         private static ScriptScope _flowScope;
         private static ScriptScope _provScope;
+        private static bool _init;
 
-        static Core()
+        private static void Init()
         {
             _engine = Python.CreateEngine();
             _flowScope = _engine.CreateScope();
             _provScope = _engine.CreateScope();
+            _init = true;
         }
 
         public static void RegisterFlowScript(string script)
         {
+            if (_init)
+                Init();
+
             _engine.Execute(script, _flowScope);
         }
 
         public static void RegisterProvisionScript(string script)
         {
+            if (_init)
+                Init();
+
             _engine.Execute(script, _provScope);
         }
 
         public static dynamic GetFlowFunction(string name)
         {
+            if (_init)
+                Init();
+
             return _flowScope.GetVariable(name);
         }
 
         public static dynamic GetProvisionFunction(string name)
         {
+            if (_init)
+                Init();
+
             return _provScope.GetVariable(name);
         }
     }
